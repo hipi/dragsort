980f0d553666da6a731eabbc1c1c2dd0030f8675
hipi <22478003+hipi@users.noreply.github.com>
2022-07-08 17:17:20 +0800

    innit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <Dragsort>
+      <DragsortItem>asdasdasdasd</DragsortItem>
+      <DragsortItem>asdasdasd</DragsortItem>
+    </Dragsort>
+  </div>
+</template>
+<script>
+import DragsortItem from './components/DragsortItem.vue'
+import Dragsort from './components/Dragsort.vue'
+export default {
+  components: { Dragsort, DragsortItem },
+}
+</script>
---
+++ src/components/DragsortItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>asdsd</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      as: '',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
