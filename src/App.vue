69988068dab7ea9d6719235cc7f177a71720c69a
hipi <22478003+hipi@users.noreply.github.com>
2024-03-06 11:15:16 +0800

    feat: 解决了一些已知问题

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,37 @@
 <template>
   <div id="app">
     {{ testData }}
-    <Dragsort :colum="3" :sort-data="testData" @sort="handleSort">
+    <Dragsort :colum="3" :sort-data.sync="testData" @sort="handleSort">
       <template slot-scope="{ item }"> {{ item }} </template>
     </Dragsort>
   </div>
 </template>
 <script>
-import Dragsort from './components/Dragsort.vue'
-import '@/assets/dragsort.scss'
+import Dragsort from "./components/Dragsort.vue";
+import "@/assets/dragsort.scss";
 export default {
   components: { Dragsort },
   data() {
     return {
       testData: [
-        { label: '1', value: 1 },
-        { label: '2', value: 2 },
-        { label: '3', value: 3 },
-        { label: '4', value: 4 },
-        { label: '5', value: 5 },
-        { label: '6', value: 6 },
+        { label: "1", value: 1 },
+        { label: "2", value: 2 },
+        { label: "3", value: 3 },
+        { label: "4", value: 4 },
+        { label: "5", value: 5 },
+        { label: "6", value: 6 },
       ],
-    }
+    };
   },
   methods: {
     handleSort(data) {
       console.log(
-        '🚀 ~ data',
+        "🚀 ~ data",
         data.map((item) => item.value)
-      )
+      );
     },
   },
-}
+};
 </script>
 
 <style scoped>
